565bc38d73a6f273a3dc9da01efb152111b4878f
Francis Hinson <francis@orchive.com>
2014-09-13 14:14:55 -0700

    updates

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,13 +1,56 @@
 $(document).ready(function(){
-  $(".word").dblclick(function () {
-    var input = $(this).text();
-    $("body").append("<audio autoplay><source src=http://tts-api.com/tts.mp3?q=" + escape(input) + " type=audio/mpeg></audio><p>\"");
-  });
-
   $(".img-brd").click(function(){
-    $(this).css("border-color", "rgba(242, 120, 75, 0.8)");
+    $(this).addClass("selected");
      $('.img-brd').not(this).each(function(){
-         $(this).css("border-color", "rgba(92, 151, 191, 0.3)")
+         $(this).removeClass("selected");
      });
   });
+});
+
+
+
+
+var DELAY = 300, clicks = 0, timer = null, wordEl;
+
+$(function(){
+    $(".word").on("click", function(e){
+
+        clicks++;  //count clicks
+        wordEl = $(this);
+        wordEl.addClass("highlight-single");
+
+        if(clicks === 1) {
+
+            timer = setTimeout(function() {
+
+                if ($(".selected").length > 0){
+                  if ($(".selected")[0].id == wordEl.text().trim()){
+                    $(".selected").addClass("animated bounceOut");
+                    wordEl.addClass("animated bounceOut");
+                    setTimeout(function(){
+                      $(".selected").parent(".col-md-3").remove();
+                      wordEl.parent(".col-md-3").remove();
+                    }, 500);
+                    
+                  }
+                  else{
+                    alert("incorrect!");
+                  }
+                }  //perform single-click action    
+                clicks = 0;             //after action performed, reset counter
+
+            }, DELAY);
+
+        } else {
+            clearTimeout(timer);    //prevent single-click action
+            wordEl.addClass("highlight-double");
+            var input = $(this).text();
+            $("body").append("<audio autoplay><source src=http://tts-api.com/tts.mp3?q=" + escape(input) + " type=audio/mpeg></audio><p>\"");  //perform double-click action
+            clicks = 0;             //after action performed, reset counter
+        }
+
+    })
+    .on("dblclick", function(e){
+        e.preventDefault();  //cancel system double-click event
+    });
 });
